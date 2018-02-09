62d6117c2ab3f2ff1930e2d638ec158d3e5c7dc0
Morichan <nintendo698@gmail.com>
2018-02-09 16:13:11 +0900

    feat: Upperクラスを追加

--- src/main/java/feature/multiplicity/Upper.java
+++ src/main/java/feature/multiplicity/Upper.java
@@ -2,15 +2,87 @@ package feature.multiplicity;
 
 import feature.value.expression.Expression;
 
+/**
+ * <p> 上限クラス </p>
+ *
+ * <p>
+ *     使い方を次に示します。
+ * </p>
+ *
+ * <pre>
+ *     {@code
+ *     Upper upperOfExpression = new Upper(new OneIdentifier(1));
+ *     System.out.println(upperOfExpression); // "1"
+ *
+ *     Upper upperOfAsterisk = new Upper("*");
+ *     System.out.println(upperOfExpression); // "*"
+ *     }
+ * </pre>
+ */
 public class Upper {
 
     private Expression expression;
+    private String asterisk;
 
-    public void setExpression(Expression exp) {
+    /**
+     * <p> 式による上限コンストラクタ </p>
+     *
+     * <p>
+     *     上限を式の{@link Expression}インスタンスで設定します。
+     * </p>
+     *
+     * @param exp 式<br>別のコンストラクタがオブジェクトを引数に取るため{@code null}不可（{@link #Upper(String)}参照）
+     */
+    public Upper(Expression exp) {
         expression = exp;
     }
 
+    /**
+     * <p> 文字列による上限コンストラクタ </p>
+     *
+     * <p>
+     *     上限を文字列の{@link Expression}インスタンスで設定します。
+     * </p>
+     *
+     * @param text 文字列<br>別のコンストラクタがオブジェクトを引数に取るため{@code null}不可（{@link #Upper(Expression)}参照）
+     */
+    public Upper(String text) {
+        if (text.length() <= 0) throw new IllegalArgumentException();
+        asterisk = text;
+    }
+
+    /**
+     * <p> 式を取得します。 </p>
+     *
+     * <p>
+     *     式が{@code null}の場合（例えば{@link #Upper(String)}によるインスタンス生成後にこのメソッドを実行した場合）には、{@link IllegalStateException}を投げます。
+     * </p>
+     *
+     * @return 式<br>{@code null}なし<br>式が{@code null}の場合は{@link IllegalStateException}
+     */
     public Expression getExpression() {
+        if (expression == null) throw new IllegalStateException();
         return expression;
+    }
+
+    /**
+     * <p> 上限の文字列を取得します。 </p>
+     *
+     * <p>
+     *     上限のインスタンス生成には必ず{@link #Upper(String)}または{@link #Upper(Expression)}を利用する必要があります。
+     *     2つのコンストラクタはどちらも{@link Object}型を引数に取るため、{@code null}を引数としたコンストラクタはコンパイルエラーとなります。
+     *     また、{@link Expression}は{@code null}不可、{@link #Upper(String)}の引数は空文字不可のため、必ず何か文字列を含んでいます。
+     * </p>
+     *
+     * @return 既定値の文字列<br>{@code null}および{@code ""}なし
+     */
+    @Override
+    public String toString() {
+        String text;
+
+        if (asterisk == null) text = expression.toString();
+        else text = asterisk;
+
+        return text;
     }
 }
--- src/test/java/feature/multiplicity/UpperTest.java
+++ src/test/java/feature/multiplicity/UpperTest.java
@@ -2,10 +2,15 @@ package feature.multiplicity;
 
 import feature.value.expression.Expression;
 import feature.value.expression.OneIdentifier;
+import net.java.quickcheck.Generator;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
+import static net.java.quickcheck.generator.PrimitiveGenerators.letterStrings;
+import static net.java.quickcheck.generator.PrimitiveGenerators.strings;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UpperTest {
@@ -18,12 +23,58 @@ class UpperTest {
         @Test
         void 設定すると式を返す() {
             String expected = "1";
-            obj = new Upper();
 
-            obj.setExpression(new OneIdentifier(1));
+            obj = new Upper(new OneIdentifier(1));
             Expression actual = obj.getExpression();
 
             assertThat(actual).hasToString(expected);
+        }
+
+        @Test
+        void 設定せずに取得しようとすると例外を投げる() {
+            obj = new Upper("expression is null");
+
+            assertThatThrownBy(() -> obj.getExpression()).isInstanceOf(IllegalStateException.class);
+        }
+    }
+
+    @Nested
+    class アスタリスクの場合 {
+        final Generator<String> textGenerator = strings();
+
+        @Test
+        void 設定するとアスタリスクを文字列として返す() {
+            String expected = "*";
+
+            obj = new Upper("*");
+            String actual = obj.toString();
+
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        void 空文字を設定すると例外を投げる() {
+            assertThatThrownBy(() -> obj = new Upper("")).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @RepeatedTest(100)
+        void アスタリスク以外を設定すると文字列を返す() {
+            String expected = selectNameWithoutAsterisk();
+
+            obj = new Upper(expected);
+            String actual = obj.toString();
+
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        private String selectNameWithoutAsterisk() {
+            String text;
+
+            do {
+                text = textGenerator.next();
+            } while (text.length() <= 0 || text.equals("*"));
+
+            return text;
         }
     }
 }
