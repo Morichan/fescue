d2a2b345df348c8ae176e90753f802f40efa4cae
Morichan <nintendo698@gmail.com>
2018-02-12 23:52:43 +0900

    feat: プロパティの1つとしてReadOnlyクラスを追加

---
+++ src/main/java/feature/property/ReadOnly.java
@@ -0,0 +1,21 @@
+package feature.property;
+
+/**
+ * <p> ReadOnlyクラス </p>
+ *
+ * <p>
+ *     プロパティにおけるReadOnlyクラスです。
+ * </p>
+ */
+public class ReadOnly {
+
+    /**
+     * <p> readOnlyプロパティの文字列を取得します。 </p>
+     *
+     * @return {@code "readOnly"}
+     */
+    @Override
+    public String toString() {
+        return "readOnly";
+    }
+}
